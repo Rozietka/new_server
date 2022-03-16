1b43a19ab9654f228cea7bba42b03a3efe9f4359
Rozietka <morkvenas.vitalijus@gmail.com>
2022-03-16 21:03:10 +0200

    detaliau

---
+++ index.js
@@ -0,0 +1,22 @@
+const app ={};
+
+app.init = () => {
+    // pasiruosti pradinius folder'ius
+
+    // pasiruosti pradinius failus
+
+    // prisijungimas prie DB (duomenu baze)
+
+    // uzkurti pati serveri (musu programa)
+    console.log('ERROR 418......');
+
+    // reguliariu procesu paleidimas
+    // - istrinti senus/nebereikalingus failus
+    // - maziau naudojamu failu archivavimas
+    // - atsinaujinti informacijos per/is API
+    
+};
+
+app.init()
+
+export { app };
